c11be83058c1dacf2e00539dda7d3ccdb6449226
章旭 <houzhangxu@163.com>
2017-06-18 01:00:10 +0800

    Update Controller
    更新了UserController与bean

--- src/com/pas/controller/UserController.java
+++ src/com/pas/controller/UserController.java
@@ -1,5 +1,27 @@
 package com.pas.controller;
 
-public class UserController {
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class UserController {
+	@RequestMapping("/shop.action")
+	public ModelAndView handleRequest1(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+//		String username = request.getParameter("username");
+		System.out.println("Output shop");
+		System.out.println("---------------------------------------------");
+		ModelAndView mv = new ModelAndView();
+		//mv.addObject("list", "123456");
+		mv.setViewName("WEB-INF/jsp/shop.jsp");
+		return mv;
+	}
 }

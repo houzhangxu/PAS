a77667223873e0b4967e47339349812520ce4679
pwq1 <1072709277@qq.com>
2017-06-18 13:14:35 +0800

    update dao

--- src/com/pas/dao/UserDao.java
+++ src/com/pas/dao/UserDao.java
@@ -1,11 +1,32 @@
 package com.pas.dao;
 
+import java.util.List;
+
 import com.pas.model.UserModel;
 
 public class UserDao implements UserDaoImp{
 
 	@Override
 	public UserModel findUserByID(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public List<UserModel> selectUser() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int findUIdByUsername(String u_id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public UserModel insertUser() {
 		// TODO Auto-generated method stub
 		return null;
 	}
